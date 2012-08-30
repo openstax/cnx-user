b97b268457b4206b6a63ac6fa2ad0de6139f8d6e
Paul Brian <paul@mikadosoftware.com>
2012-08-30 13:28:58 +0100

    initial import

---
+++ setup.py
@@ -0,0 +1,65 @@
+
+'''
+setup.py for rhaptos2
+
+'''
+
+from distutils.core import setup
+import os, glob
+
+def get_version():
+
+    '''return version from fixed always must exist file
+
+       Making very broad assumptions about the 
+       existence of files '''
+    
+    v = open('rhaptos2/user/version.txt').read().strip()
+    return v
+
+
+
+
+def main():
+
+    setup(name='rhaptos2.user',
+          version=get_version(),
+          packages=['rhaptos2.user',
+                   ],
+          namespace_packages = ['rhaptos2'],
+          author='See AUTHORS.txt',
+          author_email='info@cnx.org',
+          url='https://github.com/Connexions/rhaptos2.user',
+          license='LICENSE.txt',
+          description='User functions for rhaptos2',
+          long_description='see description',
+          install_requires=[
+              "fabric >= 1.0.0"
+              ,"flask >= 0.8"
+              ,"statsd"
+              ,"requests"
+              ,"pylint"
+              ,"python-memcached"
+              ,"nose"
+              ,"rhaptos2.common"
+              ,"unittest-xml-reporting"
+              ,"mikado.oss.doctest_additions"
+                           ],
+          scripts=glob.glob('scripts/*'),
+
+          package_data={'rhaptos2.user': ['templates/*.*', 
+                                          'static/*.*', 
+                                           'version.txt', 
+                                           'tests/*.*'],
+                        },
+
+         
+
+          
+          )
+
+
+
+if __name__ == '__main__':
+    main()
+
