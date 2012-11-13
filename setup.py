54e748efced795ff05b311b1ba16ffd297581c2d
Michael Mulich <michael.mulich@gmail.com>
2012-11-13 08:21:57 -0500

    Changes the packages keyword to use the setuptools find_packages function. Otherwise, mostly formatting adjustments.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os, glob
 
 def get_version():
     """ return a version number, or error string.
-    
+
     We are assuming a file version.txt always exists. By convention
     populate that file with output of git describe
     """
@@ -24,35 +24,30 @@ def get_version():
 
 
 setup(name='rhaptos2.user',
-          version=get_version(),
-          packages=['rhaptos2.user',
-                   ],
-          namespace_packages = ['rhaptos2'],
-          author='See AUTHORS.txt',
-          author_email='info@cnx.org',
-          url='https://github.com/Connexions/rhaptos2.user',
-          license='LICENSE.txt',
-          description='User functions for rhaptos2',
-          long_description='see description',
-          install_requires=[
-              "flask >= 0.8"
-              ,"statsd"
-              ,"requests"
-              ,"pylint"
-              ,"python-memcached"
-              ,"nose"
-              ,"rhaptos2.common"
-              ,"unittest-xml-reporting"
-              ],
-          package_data={'rhaptos2.user': ['templates/*.*', 
-                                          'static/*.*', 
-                                           'tests/*.*'],
-                        },
-    entry_points = """\
+      version=get_version(),
+      author='See AUTHORS.txt',
+      author_email='info@cnx.org',
+      url='https://github.com/Connexions/rhaptos2.user',
+      license='LICENSE.txt',
+      description='User functions for rhaptos2',
+      packages=find_packages(),
+      namespace_packages=['rhaptos2'],
+      install_requires=[
+          "flask >= 0.8",
+          "statsd",
+          "requests",
+          "pylint",
+          "python-memcached",
+          "nose",
+          "rhaptos2.common",
+          "unittest-xml-reporting",
+          ],
+      package_data={'rhaptos2.user': ['templates/*.*',
+                                      'static/*.*',
+                                      'tests/*.*'],
+                    },
+      entry_points = """\
 [console_scripts]
 run_rhaptos2user.py = rhaptos2.user.run:main
 """
-
-          )
-
-
+      )
