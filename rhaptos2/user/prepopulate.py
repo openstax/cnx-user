02bc9e4738134cd6373c6013e9e0eba64bb1b7d3
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2012-11-15 14:53:08 +0000

    Shell provides dbase shell access, prepop adds one user
    shell was doing work better named as prepopulate - that is adding a new user to the database.  Shell and prepop now also can be called out of their current location as they use abspath instead of hard coded assumption about their location

--- rhaptos2/user/prepopulate.py
+++ rhaptos2/user/prepopulate.py
@@ -13,15 +13,28 @@ import os
 from rhaptos2.user import backend, usermodel
 from rhaptos2.user.backend import db_session
 from rhaptos2.common import conf
+
 HERE = os.path.abspath(os.path.dirname(__file__))
 CONFD_PATH = os.path.join(HERE, "../../local.ini")
 confd = conf.get_config(CONFD_PATH)
 
+
 backend.initdb(confd)
 
-print "Connecting to Database:"
+print "Running simple prepopulation of database as connected by:"
 print confd['rhaptos2user']
-print "You are now in shell, without access to Flask App, but with dbase"
-print "You may want to review prepopulate.py for notes on prepopulating the dbase during development"
+
+u = usermodel.User()
+u.fullname="Test Rhaptos user"
+i = usermodel.Identifier()
+i.identifierstring = 'http://rhaptos2user.myopenid.com/'
+i.identifiertype = 'openid'
+i.user_id = u.user_id
+u.identifiers=[i,]
+
+db_session.add(u)
+db_session.commit()
+
+
 
 
