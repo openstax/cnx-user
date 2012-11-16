32cc6675a990a9cef49dd762bc866d7bde4b641b
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2012-11-16 12:44:18 +0000

    change prepopulate to add three users for searching tests

--- rhaptos2/user/prepopulate.py
+++ rhaptos2/user/prepopulate.py
@@ -18,23 +18,32 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 CONFD_PATH = os.path.join(HERE, "../../local.ini")
 confd = conf.get_config(CONFD_PATH)
 
+from rhaptos2.common.configuration import (
+    find_configuration_file,
+    Configuration,
+    )
+config = Configuration.from_file(CONFD_PATH)
 
-backend.initdb(confd)
+
+backend.initdb(config)
 
 print "Running simple prepopulation of database as connected by:"
-print confd['rhaptos2user']
+print config['pghost']
 
-u = usermodel.User()
-u.fullname="Test Rhaptos user"
-i = usermodel.Identifier()
-i.identifierstring = 'http://rhaptos2user.myopenid.com/'
-i.identifiertype = 'openid'
-i.user_id = u.user_id
-u.identifiers=[i,]
+def mkuser(fullname, openidstr):
+    u = usermodel.User()
+    u.fullname=fullname
+    i = usermodel.Identifier()
+    i.identifierstring = openidstr
+    i.identifiertype = 'openid'
+    i.user_id = u.user_id
+    u.identifiers=[i,]
 
-db_session.add(u)
-db_session.commit()
+    db_session.add(u)
+    db_session.commit()
 
+#mkuser('Rhaptos2 Test User', 'http://rhaptos2user.myopenid.com/')
+mkuser('foobar User', 'http://fake1.example.com/')
+mkuser('wibble Test User', 'http://fake2.example.com/')
 
-
-
+########
