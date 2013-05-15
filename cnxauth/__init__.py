2aff35b1877bab8889eacd941f7f6c8b65b846a7
Michael Mulich <michael.mulich@gmail.com>
2013-05-15 08:49:06 -0400

    Views reanimated and revised

--- cnxauth/__init__.py
+++ cnxauth/__init__.py
@@ -15,13 +15,20 @@ from .backend import (
     )
 
 
+def register_api(config):
+    config.add_route('identifier', '/ident/{id}')
+    config.add_route('post-user', '/users/')
+    config.add_route('get-user', '/users/{id}')
+    config.add_route('put-user', '/users/{id}')
+    config.add_route('query', '/users/query')
+
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    """This function returns a Pyramid WSGI application."""
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
     ##config.add_static_view('static', 'static', cache_max_age=3600)
+    config.include(register_api, route_prefix='/api')
     config.scan()
     return config.make_wsgi_app()
--- cnxauth/views.py
+++ cnxauth/views.py
@@ -5,161 +5,56 @@
 # Public License version 3 (AGPLv3).
 # See LICENCE.txt for details.
 # ###
-import datetime
-import md5, random
-import os, sys
-import json
-from functools import wraps
-import uuid
-import requests
-import urllib
+import logging
 
-#@app.route('/')
-def index():
-    dolog("INFO", "THis is request %s" % g.requestid)
-    return "This is the USer Database for CNX"
+from pyramid.httpexceptions import HTTPBadRequest, HTTPInternalServerError
+from pyramid.exceptions import NotFound
+from pyramid.view import view_config
+
+from . import usermodel
 
 
-#@app.route('/openid/', methods=["GET",])
-def get_user_by_identifier():
-    """
-example::
+logger = logging.getLogger('cnxauth')
 
-      /user?user=org.cnx.user.f9647df6-cc6e-4885-9b53-254aa55a3383
 
-    .. todo:: Flask will unquote the whole URL and try to route that,
-              instead of taking the route that exists in quoted format,
-               and then calculating the <parameter>
-
-              /openid/this/is/a/path
-
-              /openid/http://this/is/openid/url
-
-              /openid/'http%3A%2F%2Fthis%2Fis%2Fopenid%2Furl'
-
-              Flask should handle the last differently.
-
-    originally this use <idenfitfier> to pluckjt eh id from a path.
-
-    """
-    qstr = request.query_string
-    unquoted_identifier = qstr.replace("user=", "")
-    unquoted_identifier = urllib.unquote(unquoted_identifier)
-    dolog("INFO", "I saw identifier: %s" % unquoted_identifier)
-
+@view_config(name='identifier', request_method='GET', renderer='json')
+def get_user_by_identifier(request):
+    ident = request.matchdict['id']
     try:
-        userobj = usermodel.get_user_by_identifier(unquoted_identifier)
-        dolog("INFO", "%s <- userobj from get by identifer" % str(userobj))
+        user = usermodel.get_user_by_identifier(ident)
     except err.Rhaptos2Error, e:
-        abort(404)
-
-    resp = flask.make_response(userobj.jsonify())
-    resp.content_type='application/json'
-    return resp
+        raise NotFound()
+    return {'id': user.id, 'identifier': user.identifier, 'type': user.type}
 
 
-#@app.route('/user/<user_id>', methods=["GET",])
-def get_user(user_id):
-    """
-
-    """
+@view_config(name='get-user', request_method='GET', renderer='json')
+def get_user(request):
+    id = request.multidict['id']
     try:
-        uobj = usermodel.get_user(user_id)
+        user = usermodel.get_user(id)
     except err.Rhaptos2Error, e:
-        abort(404)
-
-    resp = flask.make_response(uobj.jsonify())
-    resp.content_type='application/json'
-    return resp
-
-#@app.route('/register/', methods=["POST","GET"])
-def vw_registration():
-    return "TBC: A registration app here"
-
-#@app.route('/user/', methods=["POST",])
-def vw_post_user():
-    """ """
+        raise NotFound()
+    return {'id': user.id, 'fullname': user.fullname, 'email': user.email}
 
 
-    js = request.json
-    ### .. todo:: parse incvoming user dict
-    dolog("INFO", "***" + repr(js) + str(type(js)))
-    u = usermodel.post_user(js)
-    return "Saved"
-
-
-#@app.route('/user/<user_id>', methods=["PUT",])
-def vw_put_user(user_id):
-    """ """
-    ###
-    #session add etc here
-    js = request.json
-    dolog("INFO", "***PUT:" + repr(js) + str(type(js)))
-    ### .. todo:: parse the posted / putted dict???
-    try:
-        u = usermodel.put_user(js, user_id)
-    except:
-        abort(401)#.. todo:: meaningful error messages to user please. Flash?
-    return "Saved"
-
-def view_all_users():
-    """ """
-    ###
-
-    rs = usermodel.get_all_users()
-    users = [u.to_dict() for u in rs]
-    json_str = json.dumps(users)
-    resp = flask.make_response(json_str)
-    resp.content_type='application/json'
-    return resp
-
-#@app.route('/users/', methods=["GET",])
-def search_user():
-    """
-    we support two modes::
-
-       GET /users/
-          (returns all users - devel only)
-       GET /users/?search=<fragment>
-          (uses a general search)
-
-    search will search many fields (currently fullname and email)
-    A new search feature willl likely allow
-
-       GET /users/?fullname=<fragment>&email=<fragment2>
-          (uses keyvalues as filters)
-
-
-    """
-
-    qstr = request.query_string
-    if qstr.strip() == '':
-        #no query, return all
-        return view_all_users()
-
-    elif "search" in request.args:
-        ###too much hardcoding
-        namefrag = request.args['search']
-
-        try:
-            matchlist = usermodel.get_user_by_name(namefrag)
-            dlist = [u.to_dict() for u in matchlist]
-        except err.Rhaptos2Error, e:
-            abort(404)
-
-        data = json.dumps(dlist)
-        callback = request.args.get('callback', None)
-        if callback is None:
-            # Send as JSON
-            content_type = 'application/json'
-        else:
-            # Send as JSONP with callback defined by request.
-            data = '{0}({1})'.format(callback, data)
-            content_type = 'application/javascript'
-
-        resp = flask.make_response(data)
-        resp.content_type = content_type
-        return resp
-
+@view_config(name='post-user', request_method=['POST', 'PUT'])
+def post_user(request):
+    id = request.multidict.get('id', None)
+    data = request.json_body
+    if request.method == 'PUT' and id is None:
+        raise HttpBadRequest("PUT without an ID")
+    elif request.metho == 'POST':
+        user = usermodel.post_user(data)
     else:
-        abort(400) #needs ?search=x syntax
+        user = usermodel.put_user(data, id)
+    return "Saved"
+
+
+@view_config(name='query', request_method='GET', renderer='json')
+def query(request):
+    q = request.query_string.get('q', '')
+    try:
+        matchlist = usermodel.get_user_by_name(q)
+    except err.Rhaptos2Error, e:
+        raise HTTPInternalServerError()
+    return [u.id for u in matchlist]
