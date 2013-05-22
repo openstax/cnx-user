d3eddfa65036b2c0d071796300dfdf0657f78ee5
Michael Mulich <michael.mulich@gmail.com>
2013-05-22 13:07:57 -0400

    Route registration functions for the web interface assets and backwards compatible api routes.

--- cnxauth/__init__.py
+++ cnxauth/__init__.py
@@ -7,6 +7,7 @@
 # ###
 """Authentication and user profile web application"""
 from pyramid.config import Configurator
+from pyramid.static import static_view
 from sqlalchemy import engine_from_config
 
 from .models import (
@@ -14,12 +15,26 @@ from .models import (
     Base,
     )
 
+def register_bbb(config):
+    """Registers the backwards compatible application programming
+    interface (API)."""
+    # TODO bring these back
+    pass
 
 def register_api(config):
+    """Registers the application programing interface (API)."""
     config.add_route('post-user', '/users/')
     config.add_route('get-user', '/users/{id}')
     config.add_route('put-user', '/users/{id}')
     config.add_route('query', '/users/query')
+
+def register_www_iface(config):
+    """Registers routes and assets for the web interface."""
+    config.add_static_view('scripts', 'assets/scripts', cache_max_age=0)
+    config.add_static_view('styles', 'assets/styles', cache_max_age=0)
+    config.add_static_view('templates', 'assets/templates', cache_max_age=0)
+    config.add_route('index', '/')
+    config.add_route('catchall', '/*path')
 
 def main(global_config, **settings):
     """This function returns a Pyramid WSGI application."""
@@ -27,7 +42,8 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
-    ##config.add_static_view('static', 'static', cache_max_age=3600)
+    config.include(register_bbb)
     config.include(register_api, route_prefix='/api')
+    config.include(register_www_iface)
     config.scan()
     return config.make_wsgi_app()
