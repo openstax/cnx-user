bc12def8afbd5313266952a6cf502c69530d3551
Michael Mulich <michael.mulich@gmail.com>
2013-06-10 15:35:51 -0400

    Include the test data in the installed distribution. Fixes #1

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     description=description,
     packages=find_packages(),
     install_requires=install_requires,
+    include_package_data=True,
     entry_points="""\
     [paste.app_factory]
     main = cnxuser:main
